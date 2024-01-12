22ccc007ed686d74e3f73d6458ee0b6dec7784aa
沈孟平 <artsmp@126.com>
2024-01-12 16:14:16 +0800

    Server.js file with dotenv configuration

---
+++ server.js
@@ -0,0 +1,7 @@
+const http = require("http");
+require("dotenv").config();
+
+const port = process.env.PORT;
+http.createServer().listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
